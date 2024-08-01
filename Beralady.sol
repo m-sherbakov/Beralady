3c17cef82e4bca70d18be18ec19d413eb759709e
SHERBAKOV <167635823+m-sherbakov@users.noreply.github.com>
2024-08-01 17:24:54 +0300

    Update Beralady.sol

--- Beralady.sol
+++ Beralady.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BeraladyT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("Beralady", "Beralady") Ownable(msg.sender) {
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI) public onlyOwner returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter++;
+        return newItemId;
+    }
+}
