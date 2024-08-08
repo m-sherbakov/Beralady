4293e2051762c9eb81aed113d9f75d7f26f9cbf9
SHERBAKOV <167635823+m-sherbakov@users.noreply.github.com>
2024-08-08 11:45:15 +0300

    Update NFTStorage.sol

--- NFTStorage.sol
+++ NFTStorage.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract NFTStorage is IERC721Receiver, Ownable {
     struct NFT {
@@ -11,36 +12,54 @@ contract NFTStorage is IERC721Receiver, Ownable {
         uint256 tokenId;
     }
 
+    IERC20 public honeyToken;
     NFT[] private nfts;
     mapping(address => bool) private hasReceivedNFT;
 
     event NFTReceived(address indexed operator, address indexed from, uint256 tokenId, bytes data);
-    event NFTIssued(address indexed to, address indexed tokenAddress, uint256 tokenId);
+    event NFTIssued(address indexed to, address indexed tokenAddress, uint256 tokenId, uint256 index);
+    event NFTNotIssued(address indexed to);
+    event HoneyWithdrawn(address indexed owner, uint256 amount);
 
-    constructor(address initialOwner) Ownable(initialOwner) {}
+    constructor(address initialOwner, address _honeyToken) Ownable(initialOwner) {
+        honeyToken = IERC20(_honeyToken);
+    }
 
-    // Функция для получения NFT. Только владелец может вызывать эту функцию.
     function receiveNFT(address tokenAddress, uint256 tokenId) external onlyOwner {
         IERC721(tokenAddress).safeTransferFrom(msg.sender, address(this), tokenId);
         nfts.push(NFT(tokenAddress, tokenId));
         emit NFTReceived(msg.sender, address(this), tokenId, "");
     }
 
-    // Функция для выдачи NFT. Любой может вызывать эту функцию.
-    function issueNFT(address to, uint256 index) external {
+    function issueNFT(address to, uint256 amountInWei, uint256 index) external {
         require(!hasReceivedNFT[to], "User has already received an NFT");
-        require(index < nfts.length, "Invalid index");
+        require(nfts.length > 0, "No NFTs available");
+        require(index < nfts.length, "Invalid NFT index");
 
-        NFT memory nft = nfts[index];
-        IERC721(nft.tokenAddress).safeTransferFrom(address(this), to, nft.tokenId);
-        emit NFTIssued(to, nft.tokenAddress, nft.tokenId);
+        require(honeyToken.transferFrom(msg.sender, address(this), amountInWei), "Token transfer failed");
 
-        // Удаление NFT из массива
-        nfts[index] = nfts[nfts.length - 1];
-        nfts.pop();
+        uint256 successChance = amountInWei * 100 / (10 * 10**18);
+        uint256 randomValue = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 100;
 
-        // Отметка, что адрес получил NFT
-        hasReceivedNFT[to] = true;
+        if (randomValue < successChance) {
+            NFT memory nft = nfts[index];
+            IERC721(nft.tokenAddress).safeTransferFrom(address(this), to, nft.tokenId);
+            emit NFTIssued(to, nft.tokenAddress, nft.tokenId, index);
+
+            // Удаляем NFT из списка, сохраняя порядок
+            nfts[index] = nfts[nfts.length - 1];
+            nfts.pop();
+
+            hasReceivedNFT[to] = true;
+        } else {
+            emit NFTNotIssued(to);
+        }
+    }
+
+    function withdrawHoney(uint256 amountInWei) external onlyOwner {
+        require(honeyToken.balanceOf(address(this)) >= amountInWei, "Insufficient balance");
+        honeyToken.transfer(owner(), amountInWei);
+        emit HoneyWithdrawn(owner(), amountInWei);
     }
 
     function onERC721Received(
@@ -63,4 +82,3 @@ contract NFTStorage is IERC721Receiver, Ownable {
         return (nft.tokenAddress, nft.tokenId);
     }
 }
-
